5c91803da2446a6bc66953ac916d43fba5320b26
James Leigh <james@3roundstones.com>
2014-12-17 09:14:12 -0500

    Fix copyright

--- object-repository/src/main/java/org/openrdf/repository/object/result/WeakValueMap.java
+++ object-repository/src/main/java/org/openrdf/repository/object/result/WeakValueMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, James Leigh Some rights reserved.
+ * Copyright (c) 2014, 3 Round Stones Inc. Some rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
